28f294a3995a04f04fd3426fd7dba83b8c022c5f
Vishnu Vekaria <vekariyavishnu63@gmail.com>
2026-04-21 11:37:08 +0100

    feat: add CORS support and JWT secret key configuration

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 from routes.user import user_bp
 from routes.analytics import analytics_bp
 from auth import auth_bp
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(user_bp)
 app.register_blueprint(analytics_bp)
 app.register_blueprint(auth_bp)
 
-
 @app.route("/health", methods=["GET"])
 def health_check():
     return jsonify({"status": "API is running"}), 200
-
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
